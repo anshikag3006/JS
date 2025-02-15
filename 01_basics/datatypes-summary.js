66c3ff9cb52173d6865fd283d31126bd53ab16d4
anshikag3006 <135628800+anshikag3006@users.noreply.github.com>
2025-02-15 08:20:27 +0000

    stand and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-// Primitive
+// Primitive => copy banti h then usme changes
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
@@ -9,16 +9,41 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// Reference (Non primitive)
+// Reference (Non primitive) => original ka reference milta h then usme changes
 
 // Array, Objects, functions
 
-const heros = ["Captain America", "doga"]
-let myObj = {
-    name: "anshika",
-    age : 20,
+// const heros = ["Captain America", "doga"]
+// let myObj = {
+//     name: "anshika",
+//     age : 20,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello")
+// }
+
+//+++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-primitive)
+
+let myYTname = "anshikadotcom"
+
+let anothername = myYTname;
+anothername = "anshikagupta3006"
+
+console.log(myYTname);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl",
 }
 
-const myFunction = function(){
-    console.log("Hello")
-}
+let userTwo = userOne
+
+userTwo.email = "anshika@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
