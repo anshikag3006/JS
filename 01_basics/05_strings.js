48333f61f8ea1bbb2e33719ac2b1a7a509fe6dee
anshikag3006 <135628800+anshikag3006@users.noreply.github.com>
2025-03-04 14:07:21 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,7 @@ const repoCount = 5
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('anshikadd')
+const gameName = new String('anshika-dd-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -13,5 +13,23 @@ const gameName = new String('anshikadd')
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('h'));
+console.log(gameName.charAt(2)); //index pr konsa char h
+console.log(gameName.indexOf('h')); //position of char
+
+const newString = gameName.substring(0, 4);
+console.log(newString); //last count nhi krta //ignores -ve sign
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "    anshika     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://anshika.com/anshika%20gupta"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('anshika'))
+
+console.log(gameName.split('-'));
