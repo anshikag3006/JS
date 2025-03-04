276432bdaf89f1ca69714bbb65d2cd4fa4a15b8d
anshikag3006 <135628800+anshikag3006@users.noreply.github.com>
2025-03-04 13:40:48 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "anshika"
+const repoCount = 5
+
+//console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('anshikadd')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
