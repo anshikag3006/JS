c8b9b4b93f73de294c9ce86f15cecd6d058530bc
anshikag3006 <135628800+anshikag3006@users.noreply.github.com>
2025-03-09 17:27:44 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,9 +1,14 @@
 // singleton
 // Object.create
+
 //object literals
+
+const mySym = Symbol("key1")
+
 
 const JsUser = {
     name: "Anshika", //key-value pair
+    [mySym]: "mySym1",
     "Full name": "Anshika Gupta",
     age: 21,
     location: "Ghaziabad",
@@ -12,6 +17,23 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["Full name"])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["Full name"])
+// console.log(JsUser[mySym]);
+
+JsUser.email = "anshika@gpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "anshika@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`); //this["name"] by this you can also access
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
