b3ba6be1b9db1e86fb64bdb8735f5c30ae0b327a
anshikag3006 <135628800+anshikag3006@users.noreply.github.com>
2025-03-30 08:00:01 +0000

    functions arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,35 @@ function loginUserMessage(username = "Sam"){
 }
 
 // console.log(loginUserMessage("anshika"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "anshika",
+    prices: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}.`);
+}
+
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price : 599
+});
+
+const myNewArray = [200, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
