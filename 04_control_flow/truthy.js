16a8bf8716ee4a50d9796ae229cd84bba9616427
anshikag3006 <135628800+anshikag3006@users.noreply.github.com>
2025-04-03 16:31:39 +0000

    Nullish Coalescing Operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -22,3 +22,22 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+
+//Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15 
+// val1 = null ?? 15 ?? 20
+// val1 = null ?? undefined //undefined
+// val1 = undefined ?? null //null
+
+console.log(val1);
+
+// Ternary Operator
+
+// Condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
